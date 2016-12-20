6d5eb129fa52b089520baa1fe7bb782ee47d9275
Carlis Liu <carlisliu@live.com>
2016-12-20 13:09:34 +0800

    extract method

--- index.js
+++ index.js
@@ -5,36 +5,37 @@ export default function wrapDefine() {
     if (!Object.getOwnPropertyDescriptor) {
         return;
     }
-
     var property = Object.getOwnPropertyDescriptor(window, 'define');
     if (!property) {
-        var define = window.define;
-        Object.defineProperty(window, 'define', {
-            get: function() {
-                return define;
-            },
-            set: function(newDefine) {
-                define = decorate(newDefine);
-            }
-        });
-        return;
+        return definition();
     }
     if (property.configurable === false) {
         return;
     }
-
     var getter = property.getter;
     var setter = property.setter;
     if (!getter || !setter) {
         return;
     }
-
     Object.defineProperty(window, 'define', {
         get: function() {
             return getter.apply(this, arguments);
         },
         set: function(define) {
             return setter.apply(this, decorate(define));
+        },
+        configurable: true
+    });
+}
+
+function definition() {
+    var define = window.define;
+    Object.defineProperty(window, 'define', {
+        get: function() {
+            return define;
+        },
+        set: function(newDefine) {
+            define = decorate(newDefine);
         }
     });
 }
